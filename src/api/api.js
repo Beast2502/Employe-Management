910b833c83911b8dfdf4245d3108b7d2bfbfae6a
BEAST2502 <mehulsaxena45@gmail.com>
2024-02-21 09:33:44 +0530

    AWS build

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,4 @@
 // export const api_end_point = 'http://localhost:8080/api'
-export const api_end_point = 'http://15.206.164.116:8080/api'
+// export const api_end_point = 'http://15.206.164.116:8080/api'
+
+export const api_end_point = 'https://19eb-2401-4900-1c3d-5fe6-307d-6e99-bace-1637.ngrok-free.app/api'
