fafbdd58783a90fc78e9516431f4ff47bcb9da1a
BEAST2502 <mehulsaxena45@gmail.com>
2024-02-21 11:13:02 +0530

    api end point

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 // export const api_end_point = 'http://localhost:8080/api'
 // export const api_end_point = 'http://15.206.164.116:8080/api'
 
-export const api_end_point = 'https://19eb-2401-4900-1c3d-5fe6-307d-6e99-bace-1637.ngrok-free.app/api'
+export const api_end_point = 'https://36ff-2401-4900-1f3c-38c-900c-7def-6ab9-92f.ngrok-free.app/api'
